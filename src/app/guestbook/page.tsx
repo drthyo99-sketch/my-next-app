447f1f1c3d05dba648ed6ee7f900ab8cf6a57349
hyo <hyo@hyoui-MacBookPro.local>
2026-01-02 01:05:42 +0900

    fix: env variable check

--- src/app/guestbook/page.tsx
+++ src/app/guestbook/page.tsx
@@ -3,10 +3,10 @@
 import { createClient } from '@supabase/supabase-js'
 import { useEffect, useState } from 'react'
 
-const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default function Guestbook() {
     const [content, setContent] = useState('')
