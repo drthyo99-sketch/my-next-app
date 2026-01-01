982cf624e6cb86171e7ea24b79260335bc7411b7
hyo <hyo@hyoui-MacBookPro.local>
2026-01-02 00:30:33 +0900

    fix: remove git commands from code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,9 +48,3 @@ export default function Home() {
     </div>
   );
 }
-git init
-git add.
-git commit - m "first commit"
-git branch - M main
-git remote add origin https://github.com/drthyo99-sketch/my-next-app.git
-git push - u origin main
